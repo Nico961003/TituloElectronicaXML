f8ed3bc70748438c37249a575186551031c66949
Nico961003 <genarorodrigueznicolas@hotmail.com>
2019-12-08 15:30:23 -0600

    Inicio de Sistema para titulos electronicos en XML con main

--- XMLFirmaElectronica/src/xmlfirmaelectronica/XMLFirmaElectronica.java
+++ XMLFirmaElectronica/src/xmlfirmaelectronica/XMLFirmaElectronica.java
@@ -15,7 +15,7 @@ public class XMLFirmaElectronica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Datos().setVisible(true);
     }
     
 }
